e172bc1e29cbdf208fa9bcfe2d1e83b962d6041c
vrinda <vrinda.sachdev2001@gmail.com>
2020-03-22 15:22:10 +0530

    assignment comleted

---
+++ module4_solution/SpeakGoodbye.js
@@ -0,0 +1,9 @@
+(function(){
+var byeSpeaker ={};
+var speakWord = "Good Bye";
+byeSpeaker.speak(name)=function() {
+  console.log(speakWord + " " + name);
+}
+window.byeSpeaker = byeSpeaker;
+	
+})(window);
